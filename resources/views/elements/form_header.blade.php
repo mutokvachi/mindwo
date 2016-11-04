4034572b5b24b4a476b6e6d7a5c8e4a3573240f6
Doitex\Janis <janis.supe@gmail.com>
2016-11-04 11:03:42 +0200

    Fixed default colors for buttons  - the same as for mindwo logo

--- resources/views/elements/form_header.blade.php
+++ resources/views/elements/form_header.blade.php
@@ -1,4 +1,4 @@
-<div class='modal-header dx-form-header' style='background-color: #31708f;'>					
+<div class='modal-header dx-form-header'>					
     <button type='button' class='close dx-form-close-btn' data-dismiss='modal' title="{{ trans('form.btn_close') }}"><i class='fa fa-times' style="color: white"></i></button>
     <h4 class='modal-title' style="color: white;">
         {{ $form_title }}
