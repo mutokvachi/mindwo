e6cefa32dd291e7fa6b907804ffb9ce95d14c0d6
Normunds Skrebelis <skrebelis.normunds@gmail.com>
2016-11-28 14:30:04 +0200

    Removed user rights check on notes list if user is manager of the employee. Hid HR notes if user was manager. Changed funcionality that user who created is owner of note - if manager creates note and hr modifies it, then user can still change Shows modified time in brackets if note has been modified

--- resources/views/profile/control_notes_record.blade.php
+++ resources/views/profile/control_notes_record.blade.php
@@ -1,14 +1,21 @@
-@if ($note->modifiedUser)
-<li class="{{ $note->modified_user_id == Auth::user()->id ? 'out' : 'in' }}" style='{{ $is_new ? 'display:none' : '' }}'>
+@if ($note->createdUser && (($has_manager_access && !$has_hr_access && !$note->is_hr) || $has_hr_access) )
+<li class="{{ $note->created_user_id == Auth::user()->id ? 'out' : 'in' }}" style='{{ $is_new ? 'display:none' : '' }}'>
     <img class="avatar dx-emp-notes-avatar" 
          alt=""  
-         src="{{Request::root()}}/{{ $note->modifiedUser->picture_guid ? 'img/' .$note->modifiedUser->picture_guid : 'assets/global/avatars/default_avatar_small.jpg' }}">
+         src="{{Request::root()}}/{{ $note->createdUser->picture_guid ? 'img/' .$note->createdUser->picture_guid : 'assets/global/avatars/default_avatar_small.jpg' }}">
     <div class="message">
         <span class="arrow"> </span>
-        <a href="{{Request::root() . '/employee/profile/' . $note->modifiedUser->id }}" class="name"> {{ $note->modifiedUser->display_name }} </a>
-        <span class="datetime"> {{ $note->modified_time->format(config('dx.txt_datetime_format')) }} </span>
+        <a href="{{Request::root() . '/employee/profile/' . $note->createdUser->id }}" class="name"> {{ $note->createdUser->display_name }} </a>
+        <span class="datetime"> {{ $note->created_time->format(config('dx.txt_datetime_format')) }} </span>
+        @if($note->created_time != $note->modified_time)
+        <span class="dx-emp-notes-modified">
+        &nbsp;({{ trans('employee.notes.modified')}}&nbsp;
+        <a href="{{Request::root() . '/employee/profile/' . $note->modifiedUser->id }}" class="name">{{ $note->modifiedUser->display_name }} </a>
+        <span class="datetime"> {{ $note->modified_time->format(config('dx.txt_datetime_format')) }}</span>)
+        </span>
+        @endif
         <span class="body dx-emp-notes-edit-body">{{ $note->note }}</span>
-        @if (($has_manager_access && Auth::user()->id == $note->modified_user_id) || $has_hr_access)
+        @if (($has_manager_access && Auth::user()->id == $note->created_user_id) || $has_hr_access)
         <div class='dx-emp-notes-btn-link-panel'>
             <input class='dx-emp-notes-edit-id' type="hidden" value='{{ $note->id }}'>
             <span>
