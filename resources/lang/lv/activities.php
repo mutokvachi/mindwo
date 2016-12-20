39972e40546c143bf9defd3987fa65beedcf09a8
Doitex\Janis <janis.supe@gmail.com>
2016-12-20 09:20:23 +0200

    Localized congratulte and activities widgets

---
+++ resources/lang/lv/activities.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels for widget "Latest activities"
+    |--------------------------------------------------------------------------
+    */   
+        
+    'widget_title' => 'PĒDĒJĀS AKTIVITĀTES', //LATEST ACTIVITIES
+    'lbl_filter' => 'Filtrēt', // Filter by
+    'lbl_other' => 'Cits', // Other
+    'btn_view' => 'Skatīt', // View
+    'btn_history' => 'Vēsture', // History
+];
