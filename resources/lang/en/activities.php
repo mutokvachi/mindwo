2fa0e0f32a27d11568debb87d1695f5cbb0e46a4
Doitex\Janis <janis.supe@gmail.com>
2016-12-22 17:58:21 +0200

    Localized activities

---
+++ resources/lang/en/activities.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels for widget "Latest activities"
+    |--------------------------------------------------------------------------
+    */   
+        
+    'widget_title' => 'LATEST ACTIVITIES', //LATEST ACTIVITIES
+    'lbl_filter' => 'Filter by', // Filter by
+    'lbl_other' => 'Other', // Other
+    'btn_view' => 'View', // View
+    'btn_history' => 'History', // History
+];
