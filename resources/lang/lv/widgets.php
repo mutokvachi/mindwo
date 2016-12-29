1107655f41e0679ceb1a05830d17699c6bf08363
Doitex\Janis <janis.supe@gmail.com>
2016-12-29 09:51:58 +0200

    Offers widget - adjusted look&feel, localization to Latvian

---
+++ resources/lang/lv/widgets.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+	'activities' => [
+		'title' => 'Latest activities',
+		'filter_by' => 'Filter by',
+		'history' => 'History',
+		'other' => 'Other',
+		'view' => 'View',
+	],
+	'congratulate' => [
+		'title' => 'Congratulate',
+	],
+	'emplbydep' => [
+		'title' => 'Employees by department',
+		'unassigned' => 'Unassigned',
+	],
+	'emplbyteam' => [
+		'title' => 'Employees by team',
+		'unassigned' => 'Unassigned',
+	],
+	'offers' => [
+		'title' => 'Piedāvājumi',
+		'subscribe' => 'Pieteikties',
+		'unsubscribe' => 'Atcelt'
+	]
+];
--- resources/views/blocks/widget_offers.blade.php
+++ resources/views/blocks/widget_offers.blade.php
@@ -1,7 +1,8 @@
+@if  (count($offers) > 0)
 <div class="portlet widget-offers" dx_block_id="offers">
   <div class="portlet-title">
     <div class="caption font-grey-cascade uppercase">{{ trans('widgets.offers.title') }}
-      <span class="badge badge-success"></span></div>
+      <span class="badge badge-success">{{ count($offers) }}</span></div>
     <div class="tools">
       <a class="collapse" href="javascript:;"> </a>
     </div>
@@ -20,15 +21,14 @@
                   </div>
                 </div>
               </div>
-              <div class="mt-action-datetime">
-                @if($offer->quantitative == 1)
+              
+              <div class="mt-action-buttons">
+                  @if($offer->quantitative == 1)
                   {{-- If offer is quantitative, display an input field --}}
                   <input id="offer_quantity_{{ $offer->id }}" type="text" class="offer-quantity form-control"
                     value="{{ $self->isSubscribed($offer) ? $self->getQuantity($offer) : '1' }}"
-                    {{ $self->isSubscribed($offer) ? 'disabled' : '' }}>
+                    {{ $self->isSubscribed($offer) ? 'disabled' : '' }} style="width: 40px; margin: 0 auto;"><br>
                 @endif
-              </div>
-              <div class="mt-action-buttons">
                 @if($self->isSubscribed($offer))
                   {{-- Unsubscribe button --}}
                   <a class="btn btn-primary green btn-xs dx-offers-unsubscribe" data-id="{{ $offer->id }}" href="javascript:;">
@@ -48,3 +48,4 @@
     </div>
   </div>
 </div>
+@endif
