947356fdfb190b6521f447237f63615c026c150a
Doitex\Janis <janis.supe@gmail.com>
2017-01-06 18:50:45 +0200

    New grid functionality - fixed headers row, added scrollbars

--- resources/views/grid/btns_col.blade.php
+++ resources/views/grid/btns_col.blade.php
@@ -1,8 +1,8 @@
 <td align='center'>
     <input type='checkbox' class='dx-grid-input-check' dx_item_id='{{ $item_id }}'>&nbsp;
     <div class="btn-group {{ $dropup }}">
-        <button type="button" class="btn btn-default dropdown-toggle btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style='color: #DDDDDD;'><i class='fa fa-cog'></i> <i class="fa fa-caret-down"></i></button>
-        <ul class="dropdown-menu dropdown-menu-right">
+        <button type="button" class="btn btn-primary dropdown-toggle btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style='color: #DDDDDD;'><i class='fa fa-cog'></i> <i class="fa fa-caret-down"></i></button>
+        <ul class="dropdown-menu dropdown-menu-right" style="z-index: 50000;">
             @if ($form_type_id == 3 && Config::get('dx.employee_profile_page_url', ''))
                 <li><a href="{{ Request::root() }}{{ Config::get('dx.employee_profile_page_url') }}{{ $item_id }}"><i class="fa fa-user"></i> {{ trans('employee.lbl_open_profile') }}</a></li>
             @else
