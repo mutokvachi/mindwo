ebe088c27c8409dec28ccc4f6089b57c5a4cae1c
Doitex\Janis <janis.supe@gmail.com>
2017-02-03 20:23:47 +0200

    Employee profile picture editing buttons design. Fixed issue with file field display (word integration case).

--- resources/views/fields/image.blade.php
+++ resources/views/fields/image.blade.php
@@ -4,15 +4,15 @@
         <a href='JavaScript: download_file({{ $item_id }}, {{ $list_id }}, {{ $field_id }});'><img src='{{Request::root()}}/formated_img/medium/{{ $file_guid }}' alt='{{ $item_value }}' style='max-height: 140px;'></a>@endif
     </div>
   </div>
-  <div class="dx-fileinput-buttons">
+  <div class="dx-fileinput-buttons" style="margin-top: 10px;">
     @if (!$is_disabled)
       <span class='btn btn-primary btn-file'>
         <span class='fileinput-new'>{{ trans('fields.btn_set') }}</span>
-        <span class='fileinput-exists'>{{ trans('fields.btn_change') }}</span>
+        <span class='fileinput-exists' title="{{ trans('fields.btn_change') }}"><i class="fa fa-pencil-square-o"></i></span>
         <input type='file' name='{{ $item_field }}' accept="{{ $ext }}"/>
         <input class='fileinput-remove-mark' type='hidden' value='0' name='{{ $item_field_remove }}'/>
       </span>
-      <a href='#' class='btn btn-white fileinput-exists' data-dismiss='fileinput'>{{ trans('fields.btn_remove') }}</a>
+    <a href='#' class='btn btn-white fileinput-exists' data-dismiss='fileinput' title="{{ trans('fields.btn_remove') }}"><i class="fa fa-trash-o"></i></a>
     @else
       <div>
         <span name='{{ $item_field }}'></span>
