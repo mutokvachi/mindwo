32cf5058d224607aa5627943928f62da2bcbccdf
Normunds Skrebelis <skrebelis.normunds@gmail.com>
2017-02-21 14:19:16 +0200

    Fix for tile resizing

--- resources/views/blocks/widget_employee_count_body.blade.php
+++ resources/views/blocks/widget_employee_count_body.blade.php
@@ -12,7 +12,7 @@
     <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100"
          aria-valuenow="{{ $counts['unassigned']['percent'] }}"
          style="width: {{ $counts['unassigned']['percent'] }}%">
-        <a href="/search?searchType={{ trans('search_top.employees') }}&amp;source_id=-1">{{ trans('widgets.emplbydep.unassigned') }} ({{ $counts['unassigned']['count'] }})</a>
+        <a href="/search?searchType={{ trans('search_top.employees') }}&amp;source_id=-1">{{ trans('widgets.employee_count.unassigned') }} ({{ $counts['unassigned']['count'] }})</a>
     </div>
 </div>
 @endif
