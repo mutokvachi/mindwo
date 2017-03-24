2c494d93a6480715e250a23828075ca525c9416d
Doitex\Janis <janis.supe@gmail.com>
2017-03-24 10:28:01 +0200

    Updates system version nr

--- resources/lang/lv/index.php
+++ resources/lang/lv/index.php
@@ -26,7 +26,7 @@ return [
     
     'login' => 'Autorizēties',
     
-    'version' => 'Versija 4.0.28, &copy; 2012 - 2017',
+    'version' => 'Versija 4.0.29, &copy; 2012 - 2017',
     
     'authorization_in_system' => 'Autorizēšanās sistēmā',
     
