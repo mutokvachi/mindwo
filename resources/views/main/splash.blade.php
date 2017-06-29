b96a7a92fa4f52b921a43f657b23926317f07c6a
Eugene Ostapenko <evo@olympsoft.com>
2017-06-29 17:02:14 +0300

    Fixed splash screen issue on page refresh in Firefox

--- resources/views/main/splash.blade.php
+++ resources/views/main/splash.blade.php
@@ -1,7 +1,7 @@
-<div class="splash">
+<div class="splash" style="position: fixed; z-index: 15000012; background: white; color: gray; top: 0; bottom: 0; left: 0; right: 0;">
   <div class="color-line"></div>
-  <div class="splash-title">
-    <h1>{{ $portal_name }}</h1>
+  <div class="splash-title" style="text-align: center; max-width: 500px; margin: 15% auto; padding: 20px;">
+    <h1 style="font-size: 26px;">{{ $portal_name }}</h1>
     <p>{{ trans("frame.data_loading") }}</p>
     <img src="{{Request::root()}}/assets/global/progress/loading-bars.svg" width="64" height="64"/>
   </div>
