6dbc1e10e89162711bd47e0aed943105f282dfe9
Doitex\Janis <janis.supe@gmail.com>
2017-07-06 08:46:43 +0300

    Fixed splash issue on FireFox in Metronic UI theme

--- resources/views/frame_metronic.blade.php
+++ resources/views/frame_metronic.blade.php
@@ -97,13 +97,7 @@
           trans_passw_form_title = "{{ trans('password_form.form_title') }}"
           >
 
-        <!-- Simple splash screen-->    
-        <div class="splash">
-            <div class="color-line"></div>
-            <div class="splash-title">
-                <h1>{{ $portal_name }}</h1><p>{{ trans("frame.data_loading") }}</p><img src="{{Request::root()}}/assets/global/progress/loading-bars.svg" width="64" height="64" />
-            </div>
-        </div>    
+        @include('main.splash')   
 
         <!-- BEGIN HEADER -->
         <div class="page-header navbar navbar-fixed-top">
