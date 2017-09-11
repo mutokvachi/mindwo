14f6b347daa52e134d20294d710ba7915212544e
Doitex\Janis <janis.supe@gmail.com>
2017-09-11 13:13:41 +0300

    Updated version nr

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -26,7 +26,7 @@ return [
     
     'login' => 'Login',
     
-    'version' => 'Version 4.0.39, &copy; 2012 - 2017',
+    'version' => 'Version 4.0.40, &copy; 2012 - 2017',
     
     'authorization_in_system' => 'Authorization in system',
     
--- resources/lang/lv/index.php
+++ resources/lang/lv/index.php
@@ -26,7 +26,7 @@ return [
     
     'login' => 'Autorizēties',
     
-    'version' => 'Versija 4.0.39, &copy; 2012 - 2017',
+    'version' => 'Versija 4.0.40, &copy; 2012 - 2017',
     
     'authorization_in_system' => 'Autorizēšanās sistēmā',
     
